e062c46594a2bd4b44a2086e19ba30765619ba26
nao20010128nao <nao20010128@gmail.com>
2016-10-25 23:25:38 +0900

    再読み込み

--- src/com/nao20010128nao/NoChatSpam/NoChatSpam.java
+++ src/com/nao20010128nao/NoChatSpam/NoChatSpam.java
@@ -6,6 +6,8 @@ import com.nao20010128nao.NoChatSpam.localevents.CommandEvent;
 import com.nao20010128nao.NoChatSpam.localevents.MeEvent;
 import com.nao20010128nao.NoChatSpam.localevents.SayEvent;
 
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
@@ -37,11 +39,46 @@ public class NoChatSpam extends PluginBase implements Listener {
 		}
 		controller.load(getServer(), this);
 		controller.onStart();
+		getServer().getCommandMap().register("nocs", new CommandDispatcher("nocs"));
 	}
 
 	@Override
 	public void onDisable() {
 		controller.onFinish();
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if ("nocs".equalsIgnoreCase(label))
+			return false;
+		if (!sender.isOp()) {
+			sender.sendMessage("You have no permission to use this command.");
+			return true;
+		}
+		if (args.length < 1) {
+			sender.sendMessage("Usage: /nocs reload");
+			return true;
+		}
+		if ("reload".equalsIgnoreCase(args[0])) {
+			sender.sendMessage("Reloading script...");
+			getLogger().info("Reloading script...");
+			reloadConfig();
+			Config config = getConfig();
+			String type = config.getString("type");
+			switch (type) {
+				case "javascript":
+				case "js":
+					controller = new JavascriptController();
+					break;
+				default:
+					getLogger().error("No type found for " + type);
+			}
+			controller.load(getServer(), this);
+			controller.onStart();
+			sender.sendMessage("Done.");
+			getLogger().info("Done.");
+		}
+		return true;
 	}
 
 	@EventHandler
@@ -66,6 +103,18 @@ public class NoChatSpam extends PluginBase implements Listener {
 				controller.onCommand(ce);
 				event.setCancelled(ce.isCancelled());
 				break;
+		}
+	}
+
+	class CommandDispatcher extends Command {
+
+		public CommandDispatcher(String name) {
+			super(name);
+		}
+
+		@Override
+		public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+			return onCommand(sender, this, commandLabel, args);
 		}
 	}
 }
